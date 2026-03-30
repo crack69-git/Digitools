af035fc522d924de3a2344daa476c6542be9e540
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-03-30 12:14:21 +0600

    added has no item cart section

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -5,7 +5,7 @@ import HasNoCart from './hasNoCart';
 const Carts = ({ cartData, setCartData, total, setTotal }) => {
 
     return (
-        <div className='w-11/12 mx-auto mt-20 border-2 border-gray-200 rounded-lg '>
+        <div className='w-11/12 mx-auto my-20 border-2 border-gray-200 rounded-lg '>
             <div className='w-11/12 mx-auto'>
                 <h3 className='pt-8 pb-5 text-2xl font-bold'>Your Cart</h3>
                 {
--- src/components/Carts/hasNoCart.jsx
+++ src/components/Carts/hasNoCart.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ShoppingCart } from 'lucide-react';
 
 const hasNoCart = () => {
     console.log('no cart');
     return (
-        <div>
-            no cart section
+        <div className='my-10'>
+            <div className='flex flex-col justify-center items-center'>
+                <ShoppingCart className='size-44 text-gray-400' />
+                <p className='text-xl text-gray-600'>Your cart is empty</p>
+            </div>
         </div>
     );
 };
